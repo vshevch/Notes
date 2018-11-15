23ea12b064a7362a440f2c7e3964c5afd5a2cf26
vshevch <vlad.shev@gmail.com>
2018-11-15 08:46:08 -0500

    Create Linux.sh

---
+++ Linux.sh
@@ -0,0 +1,7 @@
+# Linux
+-- Changing User password
+1. switching to root 'sudo su'
+2. running 'passwd <username>'
+-- Switch to another User
+sudo -i -u postgres #-i login -u username
+sudo -u postgres # Execute as another user
