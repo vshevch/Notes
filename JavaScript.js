1b3efae3c363852c44c7c362680f48015c420200
vshevch <vlad.shev@gmail.com>
2018-11-20 14:20:02 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,2 +1,8 @@
+
 // <- used for notes (1 line)
-/* */ <- used to comment out the entire text block
+/*  <- used to comment out the entire text block */
+
+// null value is repressented by null
+/*
+Undefined: This data type is denoted by the keyword undefined (without quotes). It also represents the absence of a value though it has a different use than null.
+
