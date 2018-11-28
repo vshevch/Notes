d31b3e6cfe1bb9929787df630a4361f614258566
vshevch <vlad.shev@gmail.com>
2018-11-28 14:30:44 -0500

    Update Jupyter.py

--- Jupyter.py
+++ Jupyter.py
@@ -1,2 +1,5 @@
 # Remove Scientific Notation
 pd.options.display.float_format = '{:.0f}'.format # .0f represents digits after dot
+
+# Show all columns
+pd.options.display.max_columns = None # None means no limit
