7eb9f7db67cf308c384c416567d2dc357babd147
vshevch <vlad.shev@gmail.com>
2018-11-28 11:33:22 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -3,6 +3,34 @@
 /*  <- used to comment out the entire text block */
 
 // null value is repressented by null
-/*
-Undefined: This data type is denoted by the keyword undefined (without quotes). It also represents the absence of a value though it has a different use than null.
+// Undefined: This data type is denoted by the keyword undefined (without quotes). It also represents the absence of a value though it has a different use than null.
 
+// Creating Functions
+
+function getReminder(name = 'stranger'){ // = 'stranger' is the default parameter. (Triggered if function does not receive an input)
+  console.log('Water the plants.')
+  // or
+  return (name)
+};
+
+// Concat String
+
+console.log('Thank you for your purchase' + name + '! We appreciate your business.');
+
+// Arrow Function Syntax
+
+const rectangleArea = (width, height) => {
+  let area = width * height;
+  return area
+}
+// Simplified Code
+const squareNum = num => num * num;
+
+
+// IF syntax
+if(day === 'Wednesday'){
+  return true};
+else {
+  return false}
+// Simplified Code
+day === 'Wednesday' ? true : false;
