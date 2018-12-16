0e407942a84c82f48219773be678891190571c18
vshevch <vlad.shev@gmail.com>
2018-12-16 18:06:20 -0500

    Update Linux.sh

--- Linux.sh
+++ Linux.sh
@@ -3,9 +3,12 @@
 1. switching to root 'sudo su'
 2. running 'passwd <username>'
 -- Switch to another User
+
 sudo -i -u postgres #-i login -u username
 sudo -u postgres # Execute as another user
 dpkg --list # List Installed Apps
 sudo apt-get --purge remove <app> # Remove and Purge app
 history # Show command history
 sudo netstat -plnt # Shows the active ports & programs
+ln # Make a link (use -s to make symbolic link)
+# ex. sudo ln -s /usr/bin/python3 /usr/bin/python
