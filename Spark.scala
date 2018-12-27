73bd3eafe0c3a5939c9db77b7b75d5ac2dc2af06
vshevch <vlad.shev@gmail.com>
2018-12-27 14:04:25 -0500

    Update and rename Spark.py to Spark.scala

--- Spark.scala
+++ Spark.scala
@@ -1,3 +1,11 @@
 """ -- To read an HDFS use: -- """
 rdd = sc.textFile("hdfs://surus-nameservice/data/PROD/sep/peh/20181206") # <- if it is a directory, it will read every file inside it
 # More info: https://stackoverflow.com/questions/24029873/how-to-read-multiple-text-files-into-a-single-rdd
+
+val rdd = hdfs.map(_.split("\u0001")); #<- split string which is deliminated by hexadecimal /x01
+
+#Slicing Arrays
+val test = rdd.map(array => (array.slice(0,10) ++ array.slice(12,17),array(33)))
+#
+
+
